2b5da47b79fe4d29092e17c878d7103f3f80c414
scaicedo27 <45157348+scaicedo27@users.noreply.github.com>
2019-11-20 22:09:39 -0500

    Campñas prueba 1

--- frontendEmatilde/src/app/components/client/pages_client/campaigns-client/campaigns-client.component.ts
+++ frontendEmatilde/src/app/components/client/pages_client/campaigns-client/campaigns-client.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-campaigns-client',
   templateUrl: './campaigns-client.component.html',
   styleUrls: ['./campaigns-client.component.css']
 })
